11ecf6f8215832324aa1f1919309c177253cbee7
Ardi S <ardisa2711@gmail.com>
2024-07-18 15:52:57 +0800

    feature: implement data fetch to product detail page and update data fetch in home page

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -1,11 +1,10 @@
 import { Link } from "react-router-dom";
 
 import { Card, CardContent, CardHeader } from "./ui/card";
-
 import { IProduct } from "@/utils/types/products";
 
 interface Props {
-  data: IProduct;
+  data: IProduct
   navigate: string;
 }
 
@@ -20,13 +19,13 @@ function ProductCard(props: Props) {
     <Link to={navigate}>
       <Card>
         <CardHeader>
-          <img src={data.ImageUrl} alt={data.Name} width={300} height={300} className="h-48 w-full object-contain" />
+          <img src={data.image_url} alt={data.name} width={300} height={300} className="h-48 w-full object-contain" />
         </CardHeader>
         <CardContent>
           <div className="flex flex-col items-start gap-2">
-            <h3 className="text-lg font-bold truncate w-full">{data.Name}</h3>
+            <h3 className="text-lg font-bold truncate w-full">{data.name}</h3>
             <div className="flex items-center gap-2">
-              <span className="text-2xl font-bold">{formatRupiah(data.Price)}</span>
+              <span className="text-2xl font-bold">{formatRupiah(data.price)}</span>
             </div>
           </div>
         </CardContent>
